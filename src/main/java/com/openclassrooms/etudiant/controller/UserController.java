b7506c9871a23e901acf5a8d2a5a9c1d96703f94
EwenLh <ewenlhermitte@gmail.com>
2026-09-09 14:59:29 +0200

    Modification du rendu du JWT

--- src/main/java/com/openclassrooms/etudiant/controller/UserController.java
+++ src/main/java/com/openclassrooms/etudiant/controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Map;
 
 @RestController
 @RequestMapping
@@ -30,7 +31,7 @@ public class UserController {
     @PostMapping("/api/login")
     public ResponseEntity<?> login(@Valid @RequestBody LoginRequestDTO loginRequestDTO) {
         String jwtToken = userService.login(loginRequestDTO.getLogin(), loginRequestDTO.getPassword());
-        return ResponseEntity.ok(jwtToken);
+        return ResponseEntity.ok(Map.of("token", jwtToken));
     }
 
 
